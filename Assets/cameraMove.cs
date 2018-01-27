be1c45804555ab99ba9f875b28c1f4edc634b89d
Malik BA <malik.benabda@gmail.com>
2018-01-27 23:09:03 +0100

    first push

---
+++ Assets/cameraMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour {
+	public float speed = 3f;
+	Vector2 sceanR =new Vector2 (13, 0); 
+	Vector2 sceanL =new Vector2 (-13, 0); 
+	Vector2 sceanM =new Vector2 (0, 0); 
+	Vector2 sceanU =new Vector2 (0, 10); 
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	void Update() {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector2.MoveTowards( transform.position,sceanR, step);
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate  () {
+	/*	if (Input.GetKeyDown (KeyCode.RightArrow)) {
+
+		transform.position = new Vector3 (transform.position.x + 13 *Time.deltaTime, transform.transform.position.y);
+		}
+	*/	
+	}
+}
---
+++ Assets/keypadClickManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keypadClickManager : MonoBehaviour {
+	public Camera cam;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("  clicked keypad .");		
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				// the object identified by hit.transform was clicked
+				// do whatever you want
+
+			}
+
+		
+		
+		}
+	}
+
+
+}
