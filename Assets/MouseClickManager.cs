7f8bf0c43f464ce475f20c26601197ae50367be7
Malik BA <malik.benabda@gmail.com>
2018-01-28 01:15:23 +0100

    scriptclik manager

---
+++ Assets/MouseClickManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClickManager : MonoBehaviour {
+	private Ray ray;
+	private Vector3 pos;
+	private RaycastHit2D hit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void Update () {
+		if ( Input.GetMouseButtonDown(0)){
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			hit = Physics2D.Raycast (ray.origin, ray.direction);
+			if (hit ) {
+				switch (hit.collider.gameObject.name) {
+				case "keypad": Debug.Log ("hit me   ");
+				default:					break;
+				}
+
+
+			}		
+
+		}
+
+	}
+
+
+	void doThekeypadmethod(string s){
+		
+	}
+}
